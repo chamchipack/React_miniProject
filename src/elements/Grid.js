87a2d82d25fe95caa77e19444e7249e9af460ab2
jiaegong <jiae.gong93@gmail.com>
2022-04-09 11:22:39 +0900

    Chore: Change Grid styled

--- src/elements/Grid.js
+++ src/elements/Grid.js
@@ -2,54 +2,73 @@ import React from "react";
 import styled from "styled-components";
 
 const Grid = (props) => {
-    const {is_flex, width, margin, padding, bg, children, center, _onClick, flex} = props;
+  const {
+    is_flex,
+    width,
+    margin,
+    padding,
+    bg,
+    children,
+    center,
+    _onClick,
+    flex,
+    border,
+  } = props;
 
-    const styles = {
-        is_flex: is_flex,
-        width: width,
-        padding: padding,
-        margin: margin,
-        bg: bg,
-        center: center,
-        flex: flex,
-    }
-    return (
-        <React.Fragment>
-            <GridBox {...styles} onClick={_onClick}>{children}</GridBox>
-        </React.Fragment>
-    )
-}
+  const styles = {
+    is_flex: is_flex,
+    width: width,
+    padding: padding,
+    margin: margin,
+    bg: bg,
+    center: center,
+    flex: flex,
+    border: border,
+  };
+  return (
+    <React.Fragment>
+      <GridBox {...styles} onClick={_onClick}>
+        {children}
+      </GridBox>
+    </React.Fragment>
+  );
+};
 
 Grid.defaultProps = {
-    children: null,
-    is_flex: false,
-    width: "100%",
-    padding: false,
-    margin: false,
-    bg: false,
-    center: false,
-    _onClick: () => {},
-    flex: "",
-}
+  children: null,
+  is_flex: false,
+  width: "100%",
+  padding: false,
+  margin: false,
+  bg: false,
+  center: false,
+  _onClick: () => {},
+  flex: "",
+  border: false,
+};
 
 const GridBox = styled.div`
-    width: ${(props) => props.width};
-    height: 100%;
-    box-sizing: border-box;
-    ${(props) => (props.padding? `padding: ${props.padding};`:"")}
-    ${(props) => (props.margin? `margin: ${props.margin};`:"")}
-    ${(props) => (props.bg? `background-color: ${props.bg};`:"")}
-    ${(props) => props.is_flex? `display: flex; align-items: center; justify-content: space-between;`:""}
-    ${(props) => (props.center? `text-align: center`:"")}
-    ${props =>
-    props.flex === 'start'
-      ? `display: flex; align-items: center; justify-content: start`
-      : props.flex === 'space-between'
+  width: ${(props) => props.width};
+  height: 100%;
+  box-sizing: border-box;
+  ${(props) =>
+    props.border
+      ? `border: border: 2px solid #ffb72b; border-radius: 10px;`
+      : ""}
+  ${(props) => (props.padding ? `padding: ${props.padding};` : "")}
+    ${(props) => (props.margin ? `margin: ${props.margin};` : "")}
+    ${(props) => (props.bg ? `background-color: ${props.bg};` : "")}
+    ${(props) =>
+    props.is_flex
       ? `display: flex; align-items: center; justify-content: space-between;`
-      : props.flex === 'space-around'
-      ? `display: flex; align-items: center; justify-content: space-around;`
-      : props.flex === 'end'
-      ? `display: flex; align-items: center; justify-content: end;`
+      : ""}
+    ${(props) => (props.center ? `text-align: center` : "")}
+    ${(props) =>
+    props.flex
+      ? `position: relative;
+ display: flex;
+  justify-content: center;
+  flex-direction: column;`
       : ""};
 `;
 
